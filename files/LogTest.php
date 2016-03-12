202bd87ca5fc220fcf87f73af5ab432d59fcdc03
Cameron Falkenhagen <code@twosmiles.ca>
2016-03-11 23:16:51 -0700

    add files tests

---
+++ files/LogTest.php
@@ -0,0 +1,45 @@
+<?php
+
+class LogTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function it_should_write_log_to_a_file()
+    {
+        require_once ASSIGNMENT_ROOT.'/files/functions.php';
+
+        $this->assertTrue(function_exists('log_message'), 'There is no function log_message.');
+
+        $text = $this->faker->text;
+        log_message($text);
+
+        $log = file_get_contents(ASSIGNMENT_ROOT.'/files/log.txt');
+        // clear_log();
+
+        $this->assertRegExp("/\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d $text/", $log, 'The log entry did not match the desired pattern.');
+
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_display_the_log_in_a_table()
+    {
+        clear_log();
+        $word1 = $this->faker->word;
+        $word2 = $this->faker->word;
+        log_message($word1);
+        log_message($word2);
+
+        $this->visit('files/log.php');
+
+        $tds = $this->getSession()->getPage()->findAll('css', 'td');
+
+        $this->assertRegExp("/\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d $word1/", $tds[0]->getText(), 'The log entry did not match the desired pattern.');
+
+        $this->assertRegExp("/\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d $word2/", $tds[1]->getText(), 'The log entry did not match the desired pattern.');
+    }
+
+}
