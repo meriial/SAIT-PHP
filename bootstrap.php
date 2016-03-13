b37c797d62f9d9fe417afdbffbd982b569879411
Cameron Falkenhagen <code@twosmiles.ca>
2016-03-13 17:55:34 -0600

    better organization for tests in practice and assignment folders

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 
-define('ASSIGNMENT_ROOT', '/Users/cameron/Sites/sait/php/assignment');
-define('HTTP_ROOT', 'http://localhost:8003');
+require_once 'config.php';
 
 set_include_path(get_include_path() . PATH_SEPARATOR . ASSIGNMENT_ROOT);
 
---
+++ example.config.php
@@ -0,0 +1,4 @@
+<?php
+
+define('ASSIGNMENT_ROOT', '/Users/cameron/Sites/sait/php/assignment');
+define('HTTP_ROOT', 'http://localhost:8003');
