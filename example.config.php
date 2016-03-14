df56352ada0ef0758e0df8758b4f597641e47998
Cameron Falkenhagen <code@twosmiles.ca>
2016-03-14 14:10:14 -0600

    update example config

--- example.config.php
+++ example.config.php
@@ -1,5 +1,6 @@
 <?php
 
-define('ASSIGNMENT_ROOT', '/xampp/htdocs/sait/php/assignment');
-define('PRACTICE_ROOT', '/xampp/htdocs/sait/php/practice');
+define('REPOSITORY_ROOT', '/xampp/htdocs/sait/php');
+define('ASSIGNMENT_ROOT', REPOSITORY_ROOT.'/assignment');
+define('PRACTICE_ROOT', REPOSITORY_ROOT.'/practice');
 define('HTTP_ROOT', 'http://localhost/sait/php');
