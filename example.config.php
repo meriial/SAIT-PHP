a89106000823da3d1e874ddd6d219760a35a0060
Cameron Falkenhagen <code@twosmiles.ca>
2016-03-14 13:40:16 -0600

    add PRACTICE_ROOT

--- example.config.php
+++ example.config.php
@@ -1,4 +1,5 @@
 <?php
 
 define('ASSIGNMENT_ROOT', '/Users/cameron/Sites/sait/php/assignment');
+define('PRACTICE_ROOT', '/Users/cameron/Sites/sait/php/practice');
 define('HTTP_ROOT', 'http://localhost:8003');
