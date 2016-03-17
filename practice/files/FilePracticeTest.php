65424d6f9114bd227c3eecf5ce11f28516081510
Cameron Falkenhagen <code@twosmiles.ca>
2016-03-17 12:39:16 -0600

    add file practice

---
+++ practice/files/FilePracticeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+class LogTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function it_should_write_tweets_file()
+    {
+        $this->requireOnce(PRACTICE_ROOT.'/files/functions.php');
+
+        $this->assertFunctionExists('write_tweet');
+
+        $tweet = $this->faker->text;
+        write_tweet($text);
+
+        $tweets = file_get_contents(PRACTICE_ROOT.'/files/tweets.txt');
+
+        $this->assertRegExp("/$tweet/", $tweets, "Could not find tweet $tweets .");
+    }
+
+}
